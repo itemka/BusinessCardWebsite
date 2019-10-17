eebcb882cb18225f6b2f3345f662a2bc421406a9
itemka <itemka2503@gmail.com>
2019-10-17 21:08:29 +0300

    Updates

---
+++ precache-manifest.5449daae41edd6e1aeb79052e85a91d7.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca50a268b2aa271cdd16ee249a77acbd",
+    "url": "/BusinessCardWebsite/index.html"
+  },
+  {
+    "revision": "4ce1aa0c6f62ce1085f0",
+    "url": "/BusinessCardWebsite/static/css/main.49e64df2.chunk.css"
+  },
+  {
+    "revision": "3f59f4cc4636b95408ca",
+    "url": "/BusinessCardWebsite/static/js/2.9500f32c.chunk.js"
+  },
+  {
+    "revision": "4ce1aa0c6f62ce1085f0",
+    "url": "/BusinessCardWebsite/static/js/main.13eac7b6.chunk.js"
+  },
+  {
+    "revision": "3947358ba57ca16cd1ae",
+    "url": "/BusinessCardWebsite/static/js/runtime~main.ecf92634.js"
+  },
+  {
+    "revision": "fd9ab9ee672ba92de7d809b53dbb6bdd",
+    "url": "/BusinessCardWebsite/static/media/0_U2DmhXYumRyXH6X1.fd9ab9ee.png"
+  },
+  {
+    "revision": "6b10ec6ac7fe26c58fb32d8fa81955f0",
+    "url": "/BusinessCardWebsite/static/media/16103.6b10ec6a.otf"
+  },
+  {
+    "revision": "1a0013952af11018b994867d1d4b8c90",
+    "url": "/BusinessCardWebsite/static/media/16761.1a001395.ttf"
+  },
+  {
+    "revision": "d92eb371e572f27b9750d3d1e52f5962",
+    "url": "/BusinessCardWebsite/static/media/17667.d92eb371.otf"
+  },
+  {
+    "revision": "e580be21185b24dac0717f71008e2e90",
+    "url": "/BusinessCardWebsite/static/media/17668.e580be21.otf"
+  },
+  {
+    "revision": "10043e21860d7c090e0f880086a2f8b3",
+    "url": "/BusinessCardWebsite/static/media/17728.10043e21.ttf"
+  },
+  {
+    "revision": "3a8200d9f6f60acb925a31c05b96a8cc",
+    "url": "/BusinessCardWebsite/static/media/F36ACF47-E4BD-473C-928C-6CD03CCF4B0C.3a8200d9.jpg"
+  },
+  {
+    "revision": "0f4a04fb1b9a9196107a83b2bfb742c9",
+    "url": "/BusinessCardWebsite/static/media/Laptop.0f4a04fb.mp4"
+  },
+  {
+    "revision": "c36fb89104397b5526cbf27e3e47b771",
+    "url": "/BusinessCardWebsite/static/media/Laptop.c36fb891.webm"
+  },
+  {
+    "revision": "963bb056c23b1f00a35c817021eb8d6d",
+    "url": "/BusinessCardWebsite/static/media/SocialNetwork.963bb056.png"
+  },
+  {
+    "revision": "1cde581d9fdf97325167ac820d6fb0a0",
+    "url": "/BusinessCardWebsite/static/media/ToDoList.1cde581d.png"
+  },
+  {
+    "revision": "8a35398c63fdb82a03090f102417c8ec",
+    "url": "/BusinessCardWebsite/static/media/counter.8a35398c.png"
+  },
+  {
+    "revision": "5b797b9cdc68ff4f8f2aac3cecce1c37",
+    "url": "/BusinessCardWebsite/static/media/counter2.5b797b9c.png"
+  },
+  {
+    "revision": "c0cd138271276bb33a87c2d174015455",
+    "url": "/BusinessCardWebsite/static/media/facebook.c0cd1382.svg"
+  },
+  {
+    "revision": "a3d793bb3d518f4f8eeae44e53dc6814",
+    "url": "/BusinessCardWebsite/static/media/github-logo_hub2899c31b6ca7aed8d6a218f0e752fe4_46649_1200x1200_fill_box_center_2.a3d793bb.png"
+  },
+  {
+    "revision": "0d72d2fc3a2a21662d3799bb98897948",
+    "url": "/BusinessCardWebsite/static/media/icons8-css3.0d72d2fc.svg"
+  },
+  {
+    "revision": "09d055c16ec27586da1c3f92f4f45df1",
+    "url": "/BusinessCardWebsite/static/media/icons8-html-5.09d055c1.svg"
+  },
+  {
+    "revision": "f1f848233b0981c98a2d247a96398377",
+    "url": "/BusinessCardWebsite/static/media/icons8-javascript.f1f84823.svg"
+  },
+  {
+    "revision": "2fe7d83ce1f727f1c4e072f6c9eff0df",
+    "url": "/BusinessCardWebsite/static/media/icons8-react-native.2fe7d83c.svg"
+  },
+  {
+    "revision": "b1c58ee19d1e2c38b29ae86f3174f2e4",
+    "url": "/BusinessCardWebsite/static/media/instagram.b1c58ee1.svg"
+  },
+  {
+    "revision": "4b475fb3f54b3e35daa04295ec48df4c",
+    "url": "/BusinessCardWebsite/static/media/linkedin.4b475fb3.svg"
+  },
+  {
+    "revision": "fb29301626aa48b63a102c26155d88d4",
+    "url": "/BusinessCardWebsite/static/media/phone-head.fb293016.png"
+  },
+  {
+    "revision": "bb7b30e6b128b157df21680d9256eeb8",
+    "url": "/BusinessCardWebsite/static/media/slide-bg-1.bb7b30e6.jpg"
+  },
+  {
+    "revision": "b4971d60d80ec71eec896e6b68b21bea",
+    "url": "/BusinessCardWebsite/static/media/telegram.b4971d60.svg"
+  },
+  {
+    "revision": "ef15ab6acbe9cf150de2022760229795",
+    "url": "/BusinessCardWebsite/static/media/vk.ef15ab6a.svg"
+  }
+]);
